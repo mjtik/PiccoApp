65e1fd421436c816a16b92953f5e95e3dcf8b66e
mjtik <mtikonoff@gmail.com>
2015-04-22 21:18:15 -0400

    - Changed double values for ABV and Price - Created a list of beer objects in XMLParser class

---
+++ src/Beer.java
@@ -0,0 +1,75 @@
+/**
+ * Created by mtiko_000 on 4/22/2015.
+ */
+public class Beer {
+    String price;
+    String name;
+    String style;
+    String descriptiveStyle;
+    String abv;
+    String brewery;
+    String location;
+
+    public Beer() {
+    }
+
+    public String getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStyle() {
+        return this.style;
+    }
+
+    public void setStyle(String style) {
+        this.style = style;
+    }
+
+    public String getDescriptiveStyle() {
+        return this.descriptiveStyle;
+    }
+
+    public void setDescriptiveStyle(String descriptiveStyle) {
+        this.descriptiveStyle = descriptiveStyle;
+    }
+
+    public String getAbv() {
+        return this.abv;
+    }
+
+    public void setAbv(String abv) {
+        this.abv = abv;
+    }
+
+    public String getBrewery() {
+        return this.brewery;
+    }
+
+    public void setBrewery(String brewery) {
+        this.brewery = brewery;
+    }
+
+    public String getLocation() {
+        return this.location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
+/**
+ * Created by mtiko_000 on 4/22/2015.
+ */
+
---
+++ src/PiccoApp.java
@@ -0,0 +1,15 @@
+import org.jdom2.input.SAXBuilder;
+
+/**
+ * Created by mjtik on 4/22/2015.
+ */
+public class PiccoApp {
+
+    public static void main(String [] args){
+        XMLParser parser = new XMLParser();
+
+        parser.parseXML();
+
+    }
+
+}
