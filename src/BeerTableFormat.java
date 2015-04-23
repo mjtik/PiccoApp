5f5accc547fea2f36ecf11bebe79c6ade3f14d05
mjtik <mtikonoff@gmail.com>
2015-04-22 23:26:34 -0400

    Beer list --> JTable - each column sorts - multiple sorting at the same time

---
+++ src/BeerTableFormat.java
@@ -0,0 +1,34 @@
+import ca.odell.glazedlists.gui.TableFormat;
+
+/**
+ * Created by mtiko_000 on 4/22/2015.
+ */
+public class BeerTableFormat implements TableFormat<Beer> {
+
+    public int getColumnCount() {
+        return 6;
+    }
+
+    public String getColumnName(int column) {
+        if(column == 0)      return "Name";
+        else if(column == 1) return "Style";
+        else if(column == 2) return "abv";
+        else if(column == 3) return "Brewery";
+        else if(column == 4) return "Location";
+        else if(column == 5) return "Price";
+
+        throw new IllegalStateException();
+    }
+
+    public Object getColumnValue(Beer beer, int column) {
+
+
+        if(column == 0)      return beer.getName();
+        else if(column == 1) return beer.getStyle();
+        else if(column == 2) return beer.getAbv();
+        else if(column == 3) return beer.getBrewery();
+        else if(column == 4) return beer.getLocation();
+        else if(column == 5) return beer.getPrice();
+        throw new IllegalStateException();
+    }
+}
