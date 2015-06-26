870c97c83b02f5b18b1ec6ed01d76186bc5544c4
mjtik <mtikonoff@gmail.com>
2015-06-26 14:12:17 -0500

    Flavor class changed to two separate classes for ice cream and sorbet

---
+++ src/piccoapp/IceCream.java
@@ -0,0 +1,20 @@
+package piccoapp;
+
+/**
+ * Created by mtiko_000 on 6/26/2015.
+ */
+public class IceCream {
+
+    String flavor;
+
+
+    public IceCream(String flavor) {
+
+        this.flavor = flavor;
+
+    }
+
+    public String getFlavor() {
+        return flavor;
+    }
+}
---
+++ src/piccoapp/Sorbet.java
@@ -0,0 +1,14 @@
+package piccoapp;
+
+/**
+ * Created by mtiko_000 on 6/26/2015.
+ */
+public class Sorbet {
+
+    String flavor;
+
+    public Sorbet(String flavor) {
+
+        this.flavor = flavor;
+    }
+}
